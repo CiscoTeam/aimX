692b8aa472526426a1dd5074979daa019e2f19b3
Vincent Goh <Vincent Goh>
2016-03-06 21:50:18 -0700

    Account page added

--- aimXApplication/WebContent/app/home/index.controller.js
+++ aimXApplication/WebContent/app/home/index.controller.js
@@ -3,14 +3,12 @@
 
     angular
         .module('app')
-        .controller('Account.IndexController', Controller);
+        .controller('Home.IndexController', Controller);
 
-    function Controller($window, UserService, FlashService) {
+    function Controller(UserService) {
         var vm = this;
 
         vm.user = null;
-        vm.saveUser = saveUser;
-        vm.deleteUser = deleteUser;
 
         initController();
 
@@ -19,27 +17,6 @@
             UserService.GetCurrent().then(function (user) {
                 vm.user = user;
             });
-        }
-
-        function saveUser() {
-            UserService.Update(vm.user)
-                .then(function () {
-                    FlashService.Success('User updated');
-                })
-                .catch(function (error) {
-                    FlashService.Error(error);
-                });
-        }
-
-        function deleteUser() {
-            UserService.Delete(vm.user._id)
-                .then(function () {
-                    // log user out
-                    $window.location = '/login';
-                })
-                .catch(function (error) {
-                    FlashService.Error(error);
-                });
         }
     }
 
