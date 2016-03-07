3112d1f17b9f8dd35271df03acf579ebdadc8584
cacarpe1 <cacarpe1@asu.edu>
2016-03-07 15:03:35 -0700

    changes to drag and drop

--- aimXApplication/WebContent/app/floorPlan/indexjs.js
+++ aimXApplication/WebContent/app/floorPlan/indexjs.js
@@ -3,14 +3,12 @@ var map = L.map('map', {
 	minZoom: -5
 });
 
-var bounds = [[-26.5,-25], [1000,1000]];
+var bounds = [[0,0], [1000,1000]];
 var image = L.imageOverlay('test_map_full.png', bounds).addTo(map);
 
 map.fitBounds(bounds);
 
-function allowDrop(ev) {
-    ev.preventDefault();
-}
+
 
 var greenIcon = L.icon({
     iconUrl: 'lightBulb_off.png',
@@ -35,9 +33,9 @@ var green2Icon = L.icon({
 });
 
 var marker = L.marker([-20, -20],{draggable: true,icon: greenIcon,title: "Light"},{icon: greenIcon}).addTo(map).bindPopup("<b>Lights Off</b><br>The lights are off right now");
-var marker1 = L.marker([54.5, -0.09], {draggable: true}).addTo(map).bindPopup("<b>Hi</b><br>I am a popup.");
-var marker2 = L.marker([155.5, -0.09], {draggable: true}).addTo(map).bindPopup("<b>Yo</b><br>I am a popup.");
-var marker3 = L.marker([256.5, -0.09], {draggable: true}).addTo(map).bindPopup("<b>README</b><br>I am a popup.");
+var marker1 = L.marker([54.5, -0], {draggable: true}).addTo(map).bindPopup("<b>Hi</b><br>I am a popup.");
+var marker2 = L.marker([155.5, -0], {draggable: true}).addTo(map).bindPopup("<b>Yo</b><br>I am a popup.");
+var marker3 = L.marker([-256.5, -0], {draggable: true}).addTo(map).bindPopup("<b>README</b><br>I am a popup.");
 
 
 //marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
@@ -50,8 +48,8 @@ var popup = L.popup();
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString());
-        //.openOn(map);
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
 		//marker = L.marker([-20, -20], {icon: green2Icon,draggable:"true"}).addTo(map).bindPopup("<b>Lights On</b><br>The lights are on here.");
 		//marker.setIcon(green2Icon);
 }
@@ -63,10 +61,16 @@ function changeIcon(e)
 
 marker.on("click", changeIcon);
 
-
-map.on('click', onMapClick);
-
+var mapPosition;
+function mouseDragEnd(e)
+{
+	console.log(e.latlng.toString());
+	mapPosition = e.latlng;
+}
+//map.on('click', onMapClick);
+//map.on('mousemove', mouseDragEnd);
 //drap and drop stuff
+
 
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
@@ -76,4 +80,21 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function dropOnMap(ev)
+{
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData("text");
+	var x = ev.clientX;
+	var y = ev.clientY;
+	console.log(x);
+	console.log(y);
+	//console.log(mapPosition.toString());
+	marker = L.marker(mapPosition, {icon: green2Icon,draggable:"true"}).addTo(map);//.bindPopup("<b>Lights On</b><br>The lights are on here.");
+	//popup.setLatLng(map.latlng).setContent("You clicked the map").openOn(map);
 }
