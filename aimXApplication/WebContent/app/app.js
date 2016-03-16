84e14578382bac1cf888ba0f2f23924960917c3e
Vincent Goh <Vincent Goh>
2016-03-15 21:05:29 -0700

    Worked on account database and home page

--- aimXApplication/WebContent/app/app.js
+++ aimXApplication/WebContent/app/app.js
@@ -12,7 +12,7 @@
  
         $stateProvider
             .state('home', {
-                url: '/',
+                url: '/home',
                 templateUrl: 'home/index.html',
                 controller: 'Home.IndexController',
                 controllerAs: 'vm',
@@ -24,7 +24,7 @@
                 controller: 'Account.IndexController',
                 controllerAs: 'vm',
                 data: { activeTab: 'account' }
-            });
+            })
     }
  
     function run($http, $rootScope, $window) {
--- aimXApplication/WebContent/serverLocal.js
+++ aimXApplication/WebContent/serverLocal.js
@@ -32,10 +32,10 @@ app.get('/bootstrapJSIE10',function(req,res){res.sendFile(__dirname + '/framewor
 
 // make '/app' default route
 app.get('/', function (req, res) {
-    return res.redirect('/app/home');
+    return res.redirect('/app');
 });
 
 // start server
 var server = app.listen(3000, function () {
     console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
-})
+});
