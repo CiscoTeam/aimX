39170f5f227f023803120877144becc1c1394c49
Jaime Rabago Jr <jrabago@asu.edu>
2016-04-11 21:22:48 -0700

    page set up

---
+++ aimXApplication/WebContent/app/aboutUs/index.controller.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+    angular
+        .module('app')
+        .controller('Nav.IndexController', Controller);
+		
+    function Controller(UserService) {
+        
+		var vm = this;
+
+        vm.user = null;
+
+        initController();
+        function initController() {
+            // get current user
+            UserService.GetCurrent().then(function (user) {
+                vm.user = user;
+            });
+        }
+    }
+
+})();
--- aimXApplication/WebContent/app/quickControls/index.controller.js
+++ aimXApplication/WebContent/app/quickControls/index.controller.js
@@ -3,9 +3,9 @@
     angular
         .module('app')
         .controller('Nav.IndexController', Controller);
-		
+
     function Controller(UserService) {
-        
+
 		var vm = this;
 
         vm.user = null;
@@ -18,5 +18,14 @@
             });
         }
     }
-
+    if (navigator.geolocation) { console.log('Geolocation is supported!');}
+    else { console.log('Geolocation is not supported for this Browser/OS version yet.'); }
+    window.onload = function() {
+        var startPos;
+            navigator.geolocation.getCurrentPosition(function(position) {
+                startPos = position;
+                document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+                document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+            });
+    };
 })();
