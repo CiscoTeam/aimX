3462c75032e9114d35b4787537ff9697fba8ca70
Chris Carpenter <cacarpe1@asu.edu>
2016-04-25 15:44:19 -0700

    deleted socket.io out of server.js

--- aimXApplication/WebContent/serverLocal.js
+++ aimXApplication/WebContent/serverLocal.js
@@ -1,14 +1,11 @@
 ﻿require('rootpath')();
-var express = require('express'), http = require('http');
+var express = require('express');
 var app = express();
-var server = http.createServer(app);
-
 var mongoose = require("mongoose");
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var expressJwt = require('express-jwt');
 var config = require('config.json');
-var io = require('socket.io').listen(server);
 
 mongoose.connect(config.connectionMongoose);
 
@@ -39,50 +36,14 @@ app.get('/bootstrapCSSIE10',function(req,res){res.sendFile(__dirname + '/framewo
 app.get('/bootstrapJS',function(req,res){res.sendFile(__dirname + '/frameworks/bootstrap-master/dist/js/bootstrap.min.js');});
 app.get('/bootstrapJSIE10',function(req,res){res.sendFile(__dirname + '/frameworks/bootstrap-master/docs/assets/js/ie-emulation-modes-warning.js');});
 app.get('/styleCSS',function(req,res){res.sendFile(__dirname + '/app/style.css');});
-app.get('/socket',function(req,res){res.sendFile(__dirname + '/node_modules/socket.io/lib/socket.io.js');});
 
 // make '/app' default route
 app.get('/', function (req, res) {
     return res.redirect('/app/home');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
-
-//get update from db
-/*setInterval(function() {  
-  io.sockets.emit('dbUpdate', { });
-}, 3000); //1000 = 1 second */
 
 // start server
-server.listen(3000, function () {
+var server = app.listen(3000, function () {
     console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
-})
-
-
-
-/*io.listen(server);
-
-var listener = io.listen(server);
-
-listener.sockets.on('connection', function(socket){
-  //send data to client
-  setInterval(function(){
-    //socket.emit('date', {'date': new Date()});
-  }, 1000);
-
-  //recieve client data
-  socket.on('client_data', function(data){
-    process.stdout.write(data.letter);
-  });
 });
-
-*/
-//io.sockets.on('connection', routes.vote);
-
-//server.listen(app.get('port'), function(){
-//  console.log('Express server listening on port ' + app.get('port'));
-//});
